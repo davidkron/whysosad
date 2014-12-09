52d95c119d1a6406a2e5c2caefa6c16b2a2d7b61
Jiaxin Li <lijiaxin1012@gmail.com>
2014-12-09 15:03:32 +0100

    debug

--- src/country.erl
+++ src/country.erl
@@ -14,7 +14,11 @@
 
 
 get_score(Country, TimeFrame) ->
-  db_countries:get_happiness(Country, TimeFrame) / db_countries:get_total(Country, TimeFrame).
+ Total = db_countries:get_total(Country, TimeFrame),
+ if Total == 0 -> 0;
+	true -> 	
+  db_countries:get_happiness(Country, TimeFrame) / Total
+ end.
 
 get_happiness(Country, TimeFrame) -> db_countries:get_happiness(Country, TimeFrame).
 get_total(Country, TimeFrame) -> db_countries:get_total(Country, TimeFrame).
